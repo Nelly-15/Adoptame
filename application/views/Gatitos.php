c49146b4c296c5a5ea28928504aa7ed051a66cfc
Nelly <113471834+Nelly-15@users.noreply.github.com>
2022-10-26 16:51:38 -0500

    Add files via upload

---
+++ application/views/Gatitos.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Gatitos |Pets shop</title>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
+<script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
+<style type="text/css">
+	html{
+		background-color: royalblue;
+		border: dotted white 5px;
+	}
+	.persona{
+		width: 49px;
+		color: white;
+	}
+	.menu{
+		width: 900px;
+		border: groove;
+	}
+	ion-button{
+		background-color: pink;
+		color: white;
+		border-radius: 50%;
+		border: solid white;
+		width: 340px;
+	}
+	ion-card{
+		font-family: forte;
+		font-weight: 70px;
+		width: 800px;
+		height: 290px;
+	}
+	.dog{
+		font-size: 25px;
+	}
+	.navbar{
+		background-color: black;
+		color: royalblue;
+		font-size: 40px;
+	}
+	.cat{
+		height: 150px;
+	}
+</style>
+</head>
+<ion-searchbar placeholder="Custom Placeholder" class="menu"><ion-icon name="person-circle-outline" class="persona"></ion-searchbar></ion-icon>
+<a href="#"><ion-button>Perritos</ion-button></a>
+<a href="#"><ion-button>Gatitos</ion-button></a>
+<body>
+	<section>
+		<ion-card>
+  <ion-card-header>
+    <img src="Images/miro.jpg" class="cat" align="left">
+    <ion-card-subtitle><p class="dog">Milo</ion-card-subtitle><br>
+  Gatito persa<ion-icon name="add-circle-outline"></ion-icon><br>
+Edad: 1 Año<br>
+sexo: Masculino<ion-icon name="male-outline"></ion-icon></p> 
+</ion-card>
+<ion-card>
+  <ion-card-header>
+    <img src="Images/cat.jpg" class="cat" align="left">
+    <ion-card-subtitle><p class="dog">hermanos mushu</ion-card-subtitle><br>
+  gatitos balineses<ion-icon name="add-circle-outline"></ion-icon><br>
+Edad: 1 Año<br>
+sexo: masculinos<ion-icon name="male-outline"></ion-icon></p> 
+</ion-card>
+<ion-card>
+  <ion-card-header>
+    <img src="Images/gatito.webp" class="cat" align="left">
+    <ion-card-subtitle><p class="dog">Billy</ion-card-subtitle><br>
+  Gatito persa<ion-icon name="add-circle-outline"></ion-icon><br>
+Edad: recien nacido<br>
+sexo: Femenino<ion-icon name="female-outline"></ion-icon></p> 
+<footer><nav class="navbar">
+  <a href=""><ion-icon name="home-outline"></ion-icon></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href=""><ion-icon name="heart-outline"></ion-icon></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href=""><ion-icon name="chatbubbles-outline"></ion-icon></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href=""><ion-icon name="cart-outline"></ion-icon></a>
+</nav></footer>
+
+	
+
+</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+	html{
+		border: solid 20px;
+		border-color: turquoise;
+	}
+
+	body {
+		background-image: url('https://www.segurmaniazurekin.eus/a/2020/11/segurmania_mascotas_destacada.jpg');
+		margin: 50px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: black;
+		background-color: lightpink;
+		font-weight: normal;
+		text-decoration: none;
+		font-family: forte;
+	}
+	h1 {
+		color: blueviolet;
+		border: solid black 20px;
+		background-color: white;
+		font-size: 30px;
+		text-align: center;
+		font-weight: normal;
+		margin: 4 4 14px 4;
+		padding: 17px 15px 14px 15px;
+	}
+	#container{
+		margin-top: 500px;
+		padding: 500px;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<a href="#"><h1>Bienvenido a Pets shop!</h1></a>
+</body>
+</html>
